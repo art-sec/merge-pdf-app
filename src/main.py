a632f62fc294a1d6ab1047cf4254e9e4aaf54e3a
Arthurhell <arthurfoiceiro@gmail.com>
2025-02-22 15:50:46 -0300

    Project Refactored!

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import PyPDF2
 import os
 # create the variable that contains the merger of pdf's
-merger = PyPDF2.PdfMerger
+merger = PyPDF2.PdfMerger()
 
 #create the variable that contains the pdf files and order it
 filesList = os.listdir("pdf")
@@ -9,8 +9,11 @@ filesList.sort()
 
 #create the loop that run the pdf directory and merge all the pdf files
 for file in filesList:
-    if ".pdf" in file:
-        merger.append(f"pdf/{file}")
+    if file.endswith(".pdf"): #verify if the file it's really a pdf file
+        merger.append(os.path.join("pdf", file))
 
 #create the final pdf file
-merger.write("Merger PDF.pdf")
+merger.write("Merged_PDF.pdf")
+merger.close()
+
+print("all the pdf are merged with sucess!")
